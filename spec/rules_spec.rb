a7e81c58c04a0c80681b30289a5a6b6d8b5cc990
Victor Pereira <vpereira@web.de>
2012-08-13 21:24:49 +0200

    tests improved

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -4,13 +4,17 @@ describe IDSRulesParser do
 
 	it { IDSRulesParser.new.should_not be_nil }
 
-	describe ".parse" do
+	describe "processing actions" do
 		before do
       @content = File.read(File.expand_path(File.join(File.dirname(__FILE__),"..","test","rules","test2.rules")))
 			@ids_r = IDSRulesParser.new
-	  end
+    end
 		it { @content.should_not be_nil }
-		it { @ids_r.parse(@content).should_not be_nil }
-		it { @ids_r.parse(@content).should_not be_empty }
+		describe ".parse" do
+			it { @ids_r.parse(@content).should_not be_nil }
+	  end
+		describe ".process" do
+			it { @ids_r.parse(@content).process.should_not be_empty }
+    end
   end
 end
