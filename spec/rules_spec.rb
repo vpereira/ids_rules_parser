0718704888283d3d0d9064a4d6067017b250ad38
Victor Pereira <vpereira@web.de>
2012-08-13 21:16:15 +0200

    spec improved // swp files added to gitignore

--- spec/rules_spec.rb
+++ spec/rules_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe IDSRulesParser do
-	it { true }
+
+	it { IDSRulesParser.new.should_not be_nil }
+
+	describe ".parse" do
+		before do
+      @content = File.read(File.expand_path(File.join(File.dirname(__FILE__),"..","test","rules","test2.rules")))
+			@ids_r = IDSRulesParser.new
+	  end
+		it { @content.should_not be_nil }
+		it { @ids_r.parse(@content).should_not be_nil }
+		it { @ids_r.parse(@content).should_not be_empty }
+  end
 end
